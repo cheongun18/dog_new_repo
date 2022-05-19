deafefbe4e0149d446e9f574bfa8444139778135
FlutLab.io <>
2022-05-19 08:25:21 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/dogdogapp/generatedmainwidget/GeneratedMainWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedidpwfindwidget/GeneratedIdpwfindWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedregisterwidget/GeneratedRegisterWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedmainwidget1/GeneratedMainWidget1.dart';
+import 'package:flutterapp/dogdogapp/generateduserinfowidget/GeneratedUserinfoWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedinfoeditwidget/GeneratedInfoeditWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedpwfindwidget/GeneratedPwfindWidget.dart';
+import 'package:flutterapp/dogdogapp/generateddoglistwidget/GeneratedDoglistWidget.dart';
+import 'package:flutterapp/dogdogapp/generatedlist1widget/GeneratedList1Widget.dart';
+import 'package:flutterapp/dogdogapp/generatedlist2widget/GeneratedList2Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog1widget/GeneratedDog1Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog7widget/GeneratedDog7Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog8widget/GeneratedDog8Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog5widget/GeneratedDog5Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog4widget/GeneratedDog4Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog10widget/GeneratedDog10Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog9widget/GeneratedDog9Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog6widget/GeneratedDog6Widget.dart';
+import 'package:flutterapp/dogdogapp/generatedframe8widget/GeneratedFrame8Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog13widget/GeneratedDog13Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog15widget/GeneratedDog15Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog14widget/GeneratedDog14Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog12widget/GeneratedDog12Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog11widget/GeneratedDog11Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog3widget/GeneratedDog3Widget.dart';
+import 'package:flutterapp/dogdogapp/generateddog2widget/GeneratedDog2Widget.dart';
+import 'package:flutterapp/dogdogapp/generatedlist3widget/GeneratedList3Widget.dart';
+import 'package:flutterapp/dogdogapp/generatedchatwidget/GeneratedChatWidget.dart';
+
+void main() {
+  runApp(dogdogApp());
+}
+
+class dogdogApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedMainWidget',
+      routes: {
+        '/GeneratedMainWidget': (context) => GeneratedMainWidget(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedIdpwfindWidget': (context) => GeneratedIdpwfindWidget(),
+        '/GeneratedRegisterWidget': (context) => GeneratedRegisterWidget(),
+        '/GeneratedMainWidget1': (context) => GeneratedMainWidget1(),
+        '/GeneratedUserinfoWidget': (context) => GeneratedUserinfoWidget(),
+        '/GeneratedInfoeditWidget': (context) => GeneratedInfoeditWidget(),
+        '/GeneratedPwfindWidget': (context) => GeneratedPwfindWidget(),
+        '/GeneratedDoglistWidget': (context) => GeneratedDoglistWidget(),
+        '/GeneratedList1Widget': (context) => GeneratedList1Widget(),
+        '/GeneratedList2Widget': (context) => GeneratedList2Widget(),
+        '/GeneratedDog1Widget': (context) => GeneratedDog1Widget(),
+        '/GeneratedDog7Widget': (context) => GeneratedDog7Widget(),
+        '/GeneratedDog8Widget': (context) => GeneratedDog8Widget(),
+        '/GeneratedDog5Widget': (context) => GeneratedDog5Widget(),
+        '/GeneratedDog4Widget': (context) => GeneratedDog4Widget(),
+        '/GeneratedDog10Widget': (context) => GeneratedDog10Widget(),
+        '/GeneratedDog9Widget': (context) => GeneratedDog9Widget(),
+        '/GeneratedDog6Widget': (context) => GeneratedDog6Widget(),
+        '/GeneratedFrame8Widget': (context) => GeneratedFrame8Widget(),
+        '/GeneratedDog13Widget': (context) => GeneratedDog13Widget(),
+        '/GeneratedDog15Widget': (context) => GeneratedDog15Widget(),
+        '/GeneratedDog14Widget': (context) => GeneratedDog14Widget(),
+        '/GeneratedDog12Widget': (context) => GeneratedDog12Widget(),
+        '/GeneratedDog11Widget': (context) => GeneratedDog11Widget(),
+        '/GeneratedDog3Widget': (context) => GeneratedDog3Widget(),
+        '/GeneratedDog2Widget': (context) => GeneratedDog2Widget(),
+        '/GeneratedList3Widget': (context) => GeneratedList3Widget(),
+        '/GeneratedChatWidget': (context) => GeneratedChatWidget(),
+      },
+    );
+  }
+}
